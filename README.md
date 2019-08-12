d54f4d32a57a07abc0a87f8ea0013eaa4f7f6089
Marcelo Rebouças <marcelosreboucas@gmail.com>
2019-08-12 13:41:15 -0300

    Configuração do mongo e das rotas

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Node Api to manager crushs.
 > NPM <br>
 > TypeScript <br>
 > MongoDB <br>
+
+
+
+# Files to configure Node js
+> app.ts
+> tsconfig.ts
+> server.ts
+
+# Files to configure mogodb
+> db.ts
+
+
+
+#Por ultimo vamos chamar no método construtor o método que abre nossa conexão para quando a nossa API rodar uma das primeiras coisas que será feita é a #abertura da conexão
--- server/app.ts
+++ server/app.ts
@@ -2,16 +2,21 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as bodyParser from 'body-parser';
 
+import DataBase from './config/db';
+
 class App {
 
     public app: express.Application;
     private morgan: morgan.Morgan;
     private bodyParser;
 
+    private dataBase: DataBase;
+
     constructor() {
         this.app = express();
         this.middleware();
         this.routes();
+        this.dataBase = new DataBase();
     }
 
     middleware(){
@@ -20,9 +25,19 @@ class App {
         this.app.use(bodyParser.urlencoded({ extended: true }));
       }
 
-      routes(){
+    routes(){
         this.app.route('/').get((req, res) => res.status(200).json({ 'message': 'Hello world!' }));
-      }
+        this.app.route('/game').get((req, res) => res.status(200).json({ 'message': 'Hi gamers' }));
+        this.app.route('/getic').get((req, res) => res.status(200).json({ 'message': 'Hi developres' }));
+    }
+
+    dataBaseConnection() {
+        this.dataBase.createConnection();
+    }
+
+    closeDataBaseConnection(message, callback) {
+        this.dataBase.closeConnection(message, () => callback());
+    }
 }
 
 export default new App();
