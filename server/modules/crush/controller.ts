d666f06f16974d71629ac4ecfc9774560b1037f1
Marcelo Rebouças <marcelosreboucas@gmail.com>
2019-08-13 12:57:20 -0300

    Add a controller

---
+++ server/modules/crush/controller.ts
@@ -0,0 +1,40 @@
+import Crush from './repository';
+
+class CrushController {
+    constructor() {
+
+    }
+
+    getAll() {
+        return Crush.find({
+
+        });
+    }
+
+    getByID(id) {
+        return Crush.findById(id);
+    }
+
+    create(crush) {
+        return Crush.create(crush);
+    }
+
+    update(id, crush) {
+        const updateCrush = {
+            nome: crush.nome,
+            apelido: crush.apelido,
+            whatsapp: crush.whatsapp,
+            observacoes: crush.observacoes,
+            foto: crush.foto,
+            nota: crush.nota
+          }
+
+          return Crush.findByIdAndUpdate(id, crush);
+    }
+
+    delete (id) {
+        return Crush.remove(id);
+    }
+}
+
+export default new CrushController();
